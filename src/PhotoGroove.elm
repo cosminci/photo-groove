dbf696fad270f2086dee44e999c89f0c989aeaa0
Cosmin Ciobanu <ciobanu@adobe.com>
2020-10-19 20:33:07 +0300

    Use model to render thumbnails

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,19 +1,33 @@
 module PhotoGroove exposing (main)
 
-import Html exposing (div, h1, img, text)
+import Html exposing (..)
 import Html.Attributes exposing (..)
+
+
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+viewThumbnail thumb =
+    img [ src (urlPrefix ++ thumb.filename) ] []
 
 
 view model =
     div [ class "context" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/1.jpeg" ] []
-            ]
+            (List.map viewThumbnail model.thumbnails)
         ]
 
 
+initialModel =
+    { thumbnails =
+        [ { filename = "1.jpeg" }
+        , { filename = "2.jpeg" }
+        , { filename = "3.jpeg" }
+        ]
+    }
+
+
 main =
-    view "no model yet"
+    view initialModel
