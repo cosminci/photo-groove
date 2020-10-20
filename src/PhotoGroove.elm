cfb94730ec4730b277ac8a1210fccea7165cfcfd
Cosmin Ciobanu <ciobanu@adobe.com>
2020-10-20 21:30:35 +0300

    chapter 1 complete

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,33 +1,60 @@
 module PhotoGroove exposing (main)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
 urlPrefix =
     "http://elm-in-action.com/"
 
 
-viewThumbnail thumb =
-    img [ src (urlPrefix ++ thumb.filename) ] []
+viewThumbnail selectedThumb thumb =
+    img
+        [ src (urlPrefix ++ thumb.fileName)
+        , classList [ ( "selected", selectedThumb == thumb.fileName ) ]
+        , onClick { action = constants.clickedPhoto, target = thumb.fileName }
+        ]
+        []
 
 
 view model =
     div [ class "context" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            (List.map viewThumbnail model.thumbnails)
+            (List.map (viewThumbnail model.selectedThumbnail)
+                model.thumbnails
+            )
+        , img [ class "large", src (urlPrefix ++ "large/" ++ model.selectedThumbnail) ] []
         ]
+
+
+constants =
+    { clickedPhoto = "clicked-photo" }
 
 
 initialModel =
     { thumbnails =
-        [ { filename = "1.jpeg" }
-        , { filename = "2.jpeg" }
-        , { filename = "3.jpeg" }
+        [ { fileName = "1.jpeg" }
+        , { fileName = "2.jpeg" }
+        , { fileName = "3.jpeg" }
         ]
+    , selectedThumbnail = "1.jpeg"
     }
 
 
+update msg model =
+    if msg.action == constants.clickedPhoto then
+        { model | selectedThumbnail = msg.target }
+
+    else
+        model
+
+
 main =
-    view initialModel
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
