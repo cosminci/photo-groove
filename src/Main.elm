97e989a4909d8a2004de95c300f7bcfc36d6b798
Cosmin Ciobanu <ciobanu@adobe.com>
2020-11-02 01:08:24 +0200

    SPA scaffold + basic nav bar

---
+++ src/Main.elm
@@ -0,0 +1,81 @@
+module Main exposing (main)
+
+import Browser exposing (Document)
+import Html exposing (Html, a, footer, h1, li, nav, text, ul)
+import Html.Attributes exposing (classList, href)
+
+
+type alias Model =
+    { page : Page }
+
+
+type Page
+    = Gallery
+    | Folders
+    | NotFound
+
+
+view : Model -> Document Msg
+view model =
+    let
+        content =
+            text "This isn’t even my final form!"
+    in
+    { title = "Photo Groove, SPA Style"
+    , body =
+        [ viewHeader model.page
+        , content
+        , viewFooter
+        ]
+    }
+
+
+viewHeader : Page -> Html Msg
+viewHeader currentPage =
+    let
+        logo =
+            h1 [] [ text "Photo Groove" ]
+
+        links =
+            ul []
+                [ navLink Folders { url = "/", caption = "Folders" }
+                , navLink Gallery { url = "/gallery", caption = "Gallery" }
+                ]
+
+        navLink : Page -> { url : String, caption : String } -> Html msg
+        navLink targetPage { url, caption } =
+            li [ classList [ ( "active", currentPage == targetPage ) ] ]
+                [ a [ href url ] [ text caption ] ]
+    in
+    nav [] [ logo, links ]
+
+
+viewFooter : Html msg
+viewFooter =
+    footer []
+        [ text "One is never alone with a rubber duck. -Douglas Adams"
+        ]
+
+
+type Msg
+    = NothingYet
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main : Program () Model Msg
+main =
+    Browser.document
+        { init = \_ -> ( { page = Gallery }, Cmd.none )
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        }
